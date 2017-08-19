c40957077c5dcdf15ab6266099c699bc08de7711
Christian Wiedehöft <wiedehoeft.christian@gmail.com>
2017-08-19 10:47:31 +0000

    Base structure with bootstrap created

--- js/spa.js
+++ js/spa.js
@@ -1,13 +1,19 @@
-var spa = (function() {
-    console.log("Calling init")
-    var initModule = function() {
-        console.log(document.getElementById("spa"))
-        document.getElementById("spa").innerHtml +=
-            '<h1 style="display:inline-block; margin: 25px;">'
-            + 'hello world!'
-            + '</h1>'
-    }
-    return {
-        initModule: initModule
-    }
-}())
+$('body').html(
+    '<div class="container-fluid">'    +
+        '<div class ="row">'+
+            '<div class="col-sm-2">Logo</div>'+
+            '<div class="col-sm-8"><h1>My personal ToDoList</p></div>'         +
+        '</div>'+
+    '</div>'                           +
+    
+    '<div class =container>'+
+        '<div class="card" style="width: 20rem;">' +
+            '<img class="card-img-top" src="..." alt="Card image cap">'+
+            '<div class="card-body">'+
+                '<h4 class="card-title">Card title</h4>'+
+                '<textarea id="cardInput" class="card-text" placeholder="Put your task description here"></textarea>'+
+                '<a href="#" class="btn btn-primary">Go somewhere</a>'+
+            '</div>'+
+        '</div>'+
+    '<div>'
+)
